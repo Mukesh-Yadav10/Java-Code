8e8de62ff9ee5f27affab32e3f2e89adf3278770
Mukesh Yadav <your.email@example.com>
2025-09-10 21:26:37 +0530

    operator

---
+++ ArithmaticOperator.java
@@ -0,0 +1,10 @@
+public class ArithmaticOperator {
+    public static void main(String[] args) {
+        int x=10,y=5;
+        System.out.println("the sum of two number is "+(x+y));
+        System.out.println("the subtraction of two number is "+(x-y));
+        System.out.println("the multipliction of two number is "+(x*y));
+        System.out.println("the division of two number is "+(x/y));
+        System.out.println("the modulus of two number is "+(x%y));
+    }
+}
